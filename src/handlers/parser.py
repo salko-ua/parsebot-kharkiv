d2b9ee5a9ad97cbdcb148ed35c7d595db0ab70cb
SALO <github@salko-ua.de>
2025-09-01 19:18:49 +0300

    Update tag extraction logic in parser.py

--- src/handlers/parser.py
+++ src/handlers/parser.py
@@ -47,7 +47,7 @@ def get_tag(soup: BeautifulSoup) -> [int, int, str]:
         "Поверховість:",
     ]
     # parsing tags from the page
-    tags = soup.find("div", class_="css-1g2c38u").find_all("p")
+    tags = self.soup.find("div", {"data-testid": "ad-parameters-container"}).find_all("p")
     all_tag_text = []
 
     for need_word in NEED_WORDS_RUSSIAN:
